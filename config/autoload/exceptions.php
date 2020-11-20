864b4db840fb4df9b20a50fa0cec07320f466f00
deno <inbox@email.com>
2020-11-20 14:56:23 +0800

    upgrade

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -13,7 +13,7 @@ return [
     'handler' => [
         'http' => [
             Hyperf\HttpServer\Exception\Handler\HttpExceptionHandler::class,
-            App\Exception\Handler\AppExceptionHandler::class,
+            App\Exception\Handler::class,
         ],
     ],
 ];
