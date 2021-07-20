e004367ba634df059edb213d61110f5096fc2979
wanganlin <inbox@wanganlin.com>
2021-07-20 08:52:12 +0800

    remove default time zone

--- routes/route.php
+++ routes/route.php
@@ -7,3 +7,7 @@ use Hyperf\HttpServer\Router\Router;
 Router::get('/monitor', function () {
     return json_encode(['heartbeat' => time()]);
 });
+
+Router::get('/favicon.ico', function () {
+    return '';
+});
