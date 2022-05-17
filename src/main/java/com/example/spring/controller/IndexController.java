e162373aba39c252ec5a3f3137ca1e281a39cbb9
wanganlin <inbox@wanganlin.com>
2022-05-17 11:22:23 +0800

    formatting

--- src/main/java/com/example/spring/controller/IndexController.java
+++ src/main/java/com/example/spring/controller/IndexController.java
@@ -1,8 +1,5 @@
 package com.example.spring.controller;
 
-import com.example.spring.config.WebConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
