f1425d8fe0cb1649c9ca535dced81d5e3ea607ae
wanganlin <inbox@wanganlin.com>
2022-05-24 10:52:44 +0800

    配置mybatis

---
+++ src/main/java/com/example/spring/mapper/MyMapper.java
@@ -0,0 +1,7 @@
+package com.example.spring.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+}
