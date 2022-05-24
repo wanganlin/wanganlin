1886355d6ed01528f6bbb0fa0736cd771afe5ce8
wanganlin <inbox@wanganlin.com>
2022-05-24 10:06:44 +0800

    增加异步任务

--- src/main/java/com/example/spring/controller/IndexController.java
+++ src/main/java/com/example/spring/controller/IndexController.java
@@ -1,13 +1,24 @@
 package com.example.spring.controller;
 
+import com.example.spring.scheduler.MyAsync;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
+@Slf4j
 public class IndexController {
+
+    @Autowired
+    MyAsync myAsync;
 
     @GetMapping("/")
     public String index() {
+        myAsync.testLog();
+        log.warn("同步执行时间：" + LocalDateTime.now());
         return "hello imooc";
     }
 
