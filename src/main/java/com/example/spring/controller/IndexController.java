34927b5e7aec46b42a43283c31f65f460afe5163
wanganlin <inbox@wanganlin.com>
2022-05-26 09:08:46 +0800

    Thymeleaf 处理字符串与日期格式化

--- src/main/java/com/example/spring/controller/IndexController.java
+++ src/main/java/com/example/spring/controller/IndexController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Controller
 @Slf4j
@@ -21,7 +24,20 @@ public class IndexController {
         myAsync.testLog();
         log.warn("同步执行时间：" + LocalDateTime.now());
 
-        model.addAttribute("varName", "haha model varName");
+        String varName = "haha model varName";
+        model.addAttribute("varName", varName);
+
+        LocalDate now = LocalDate.now();
+        model.addAttribute("now", now);
+
+        LocalDateTime time = LocalDateTime.now();
+        model.addAttribute("time", time);
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd");
+        model.addAttribute("timeFormat", time.format(dateTimeFormatter));
+
+        Date date = new Date();
+        model.addAttribute("date", date);
 
         return "index";
     }
