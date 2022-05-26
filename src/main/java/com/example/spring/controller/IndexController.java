cc031001b456d4e2eee058924a0fa090cfc7fb2e
wanganlin <inbox@wanganlin.com>
2022-05-26 08:48:31 +0800

    入门Thymeleaf模板与基本使用

--- src/main/java/com/example/spring/controller/IndexController.java
+++ src/main/java/com/example/spring/controller/IndexController.java
@@ -3,12 +3,13 @@ package com.example.spring.controller;
 import com.example.spring.scheduler.MyAsync;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@RestController
+@Controller
 @Slf4j
 public class IndexController {
 
@@ -16,10 +17,13 @@ public class IndexController {
     MyAsync myAsync;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
         myAsync.testLog();
         log.warn("同步执行时间：" + LocalDateTime.now());
-        return "hello imooc";
+
+        model.addAttribute("varName", "haha model varName");
+
+        return "index";
     }
 
 }
