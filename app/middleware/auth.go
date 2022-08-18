09eec8d30be05c5da8c969fe0f383fabb01c0082
wanganlin <inbox@wanganlin.com>
2022-08-18 22:02:37 +0800

    add middleware

--- app/middleware/auth.go
+++ app/middleware/auth.go
@@ -1,16 +1,20 @@
 package middleware
 
 import (
+	"github.com/gogf/gf/v2/encoding/gurl"
 	"github.com/gogf/gf/v2/net/ghttp"
-	"net/http"
 )
 
-func Auth(r *ghttp.Request) {
-	token := r.Get("token")
-	if token.String() == "123456" {
-		r.Response.Writeln("auth")
+// Auth 认证中间件
+func Auth(guard string) func(r *ghttp.Request) {
+	return func(r *ghttp.Request) {
+		if auth, _ := r.Session.Get(guard + "_auth"); auth == nil {
+			callback := r.Request.RequestURI
+			if callback != "" {
+				callback = "?callback=" + gurl.Encode(callback)
+			}
+			r.Response.RedirectTo("/" + guard + "/login" + callback)
+		}
 		r.Middleware.Next()
-	} else {
-		r.Response.WriteStatus(http.StatusForbidden)
 	}
 }
