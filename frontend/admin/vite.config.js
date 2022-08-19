8a8453ee5d8729813bc328944bd4db16f280627e
wanganlin <inbox@wanganlin.com>
2022-08-19 09:22:16 +0800

    update frontend config

--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // mode: 'production',
-  base: '/admin/',
+  base: '/static/admin/',
   build: {
     outDir: '../../public/static/admin'
   },
--- frontend/user/vite.config.js
+++ frontend/user/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // mode: 'production',
-  base: '/user/',
+  base: '/static/user/',
   build: {
     outDir: '../../public/static/user'
   },
