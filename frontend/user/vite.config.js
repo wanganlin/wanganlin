43088eb77912601272c63dd182d02964c25e1f78
wanganlin <inbox@wanganlin.com>
2022-08-19 08:36:16 +0800

    add frontend

---
+++ frontend/user/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // mode: 'production',
+  base: '/user/',
+  build: {
+    outDir: '../../public/static/user'
+  },
+  plugins: [react()]
+})
