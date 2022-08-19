863323f6f01a514ae18a37a8c9ae83236a698675
wanganlin <inbox@wanganlin.com>
2022-08-19 13:56:11 +0800

    add database

--- app/controller/user/auth.go
+++ app/controller/user/auth.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/wanganlin/goframe/app/request/user"
+	"github.com/wanganlin/goframe/app/service"
 )
 
 type cAuth struct{}
@@ -12,6 +13,11 @@ type cAuth struct{}
 var Auth = cAuth{}
 
 func (a *cAuth) Login(ctx context.Context, req *user.LoginReq) (res *user.LoginRes, err error) {
-	g.RequestFromCtx(ctx).Response.WriteJson(g.Map{"a": "b"})
+	if out, err := service.User().FindById(ctx, 1); err != nil {
+		return nil, err
+	} else {
+		g.RequestFromCtx(ctx).Response.WriteJson(out)
+	}
+
 	return
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 	_ "github.com/wanganlin/goframe/app/service/logic"
 
 	"github.com/gogf/gf/v2/os/gctx"
