488cd2b6a71226326e4c4c18dc6959374a97f2ee
wanganlin <wanganlin21@qq.com>
2022-11-06 20:34:24 +0800

    增加注解路由

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\RouteDiscovery\Discovery\Discover;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Discover::controllers()->in(app_path('Http/Controllers'));
