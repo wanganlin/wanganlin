9763bf31ded898010b5aa8556ec662f8f9b20d7a
Wal <gosoft@foxmail.com>
2022-11-16 14:14:38 +0800

    更新数据迁移

--- database/migrations/m221115_084841_create_role_table.php
+++ database/migrations/m221115_084841_create_role_table.php
@@ -2,11 +2,10 @@
 
 use yii\db\Migration;
 
-class m221115_084845_create_role_table extends Migration
+class m221115_084841_create_role_table extends Migration
 {
     public function safeUp()
     {
-
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
