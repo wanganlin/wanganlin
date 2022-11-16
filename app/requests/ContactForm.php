2ebb867b303187301358fbd59ed64dc70000d0d5
Wal <gosoft@foxmail.com>
2022-11-16 13:06:48 +0800

    增加方法返回类型

--- app/requests/ContactForm.php
+++ app/requests/ContactForm.php
@@ -20,7 +20,7 @@ class ContactForm extends Model
     /**
      * @return array the validation rules.
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             // name, email, subject and body are required
@@ -35,7 +35,7 @@ class ContactForm extends Model
     /**
      * @return array customized attribute labels
      */
-    public function attributeLabels()
+    public function attributeLabels(): array
     {
         return [
             'verifyCode' => 'Verification Code',
@@ -47,7 +47,7 @@ class ContactForm extends Model
      * @param string $email the target email address
      * @return bool whether the model passes validation
      */
-    public function contact($email)
+    public function contact(string $email): bool
     {
         if ($this->validate()) {
             Yii::$app->mailer->compose()
