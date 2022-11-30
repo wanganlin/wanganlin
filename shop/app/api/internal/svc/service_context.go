d42659621c41c7a22c4fa8e8723b5d5f48eeb09f
Wal <gosoft@foxmail.com>
2022-11-30 15:22:56 +0800

    增加shop模块

---
+++ shop/app/api/internal/svc/service_context.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"gosoft/shop/app/api/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
---
+++ shop/app/console/internal/svc/service_context.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"gosoft/shop/app/console/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
