975152c93f2697ad795c50b25420786d848f9765
wanganlin <inbox@wanganlin.com>
2023-01-12 15:30:43 +0800

    增加前台主题模板

---
+++ public/themes/default/article.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="zh-Hans">
+<head>
+    <meta charset="utf-8">
+    <title>article</title>
+    <link rel="stylesheet" href="{:asset('assets/layui/dist/css/layui.css')}">
+    <link rel="stylesheet" href="{:theme('css/app.css')}">
+    <script src="{:asset('assets/layui/dist/layui.js')}"></script>
+    <script src="{:asset('assets/vue/dist/vue.min.js')}"></script>
+    <script src="{:theme('js/app.js')}"></script>
+</head>
+<body>
+article
+</body>
+</html>
