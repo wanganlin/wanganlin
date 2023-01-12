c6af995850a9ba627ed67ae52171a7a0dafa897f
wanganlin <inbox@wanganlin.com>
2023-01-12 15:28:48 +0800

    增加中间件

---
+++ app/middleware/Authenticate.php
@@ -0,0 +1,31 @@
+<?php
+
+declare (strict_types=1);
+
+namespace app\middleware;
+
+use Closure;
+use think\Request;
+use think\Response;
+
+class Authenticate
+{
+    /**
+     * 处理请求
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @param string $guard
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next, string $guard): Response
+    {
+        if (!session('?auth_' . $guard)) {
+            $url = $guard . '/auth/login?callback=';
+            $callback = urlencode($request->url(true));
+            return redirect($url . $callback);
+        }
+
+        return $next($request);
+    }
+}
