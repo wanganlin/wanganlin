3afd157165023ca935e04bc15fbee19fd77c3806
wanganlin <inbox@wanganlin.com>
2023-01-12 15:32:59 +0800

    增加请求认证类

---
+++ app/request/console/auth/LoginRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\request\console\auth;
+
+use think\Validate;
+
+/**
+ * Class LoginRequest
+ */
+class LoginRequest extends Validate
+{
+    /**
+     * @var array
+     */
+    protected $rule = [
+        'passport' => 'require',
+        'password' => 'require',
+        'captcha|验证码' => 'require|captcha'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $message = [
+        'passport.require' => '登录用户名必须',
+        'password.require' => '登录密码必须',
+        'captcha.require' => '图片验证码必须',
+    ];
+}
