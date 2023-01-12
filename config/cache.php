02ec50f6c481cf773017c265e1789c877c3aaaae
wanganlin <inbox@wanganlin.com>
2023-01-12 13:16:44 +0800

    更新配置

--- config/cache.php
+++ config/cache.php
@@ -2,23 +2,23 @@
 
 return [
     // 默认缓存驱动
-    'default' => env('cache.driver', 'file'),
+    'default' => env('CACHE_DRIVER', 'file'),
 
     // 缓存连接方式配置
-    'stores'  => [
+    'stores' => [
         'file' => [
             // 驱动方式
-            'type'       => 'File',
+            'type' => 'File',
             // 缓存保存目录
-            'path'       => '',
+            'path' => '',
             // 缓存前缀
-            'prefix'     => '',
+            'prefix' => '',
             // 缓存有效期 0表示永久缓存
-            'expire'     => 0,
+            'expire' => 0,
             // 缓存标签前缀
             'tag_prefix' => 'tag:',
             // 序列化机制 例如 ['serialize', 'unserialize']
-            'serialize'  => [],
+            'serialize' => [],
         ],
         // 更多的缓存连接
     ],
--- config/session.php
+++ config/session.php
@@ -2,15 +2,15 @@
 
 return [
     // session name
-    'name'           => 'PHPSESSID',
+    'name' => 'PHPSESSID',
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
     // 驱动方式 支持file cache
-    'type'           => 'file',
+    'type' => env('SESSION_DRIVER', 'file'),
     // 存储连接标识 当type使用cache的时候有效
-    'store'          => null,
+    'store' => null,
     // 过期时间
-    'expire'         => 1440,
+    'expire' => env('SESSION_LIFETIME', 1440),
     // 前缀
-    'prefix'         => '',
+    'prefix' => '',
 ];
