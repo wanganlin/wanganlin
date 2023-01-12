fed261ac2cafdcf728f07a7a142a7e3b269e561a
wanganlin <inbox@wanganlin.com>
2023-01-12 13:26:31 +0800

    增加自动路由

--- route/app.php
+++ route/app.php
@@ -2,4 +2,14 @@
 
 use think\facade\Route;
 
-Route::get('hello', 'index/index');
+$dirs = glob(app_path('controller/*'), GLOB_ONLYDIR);
+foreach ($dirs as $dir) {
+    $m = basename($dir);
+    $v = $m === config('app.default_app') ? '' : $m;
+    Route::group($v, function () {
+        Route::get(':c/:a', ':c/:a');
+        Route::post(':c/:a', ':c/:a');
+        Route::get(':c', ':c/index');
+        Route::get('/', 'Index/index');
+    })->prefix($m.'.');
+}
