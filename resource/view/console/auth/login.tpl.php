83dd5b23da4e038ed04dd2d4f1324fbad01e41ff
wanganlin <inbox@wanganlin.com>
2023-01-12 19:15:39 +0800

    增加模板

--- resource/view/console/auth/login.tpl.php
+++ resource/view/console/auth/login.tpl.php
@@ -1,10 +1,71 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-</head>
-<body>
-login page.
-</body>
+<html lang="zh-Hans">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<title>登录</title>
+        <link rel="stylesheet" href="{:asset('assets/layui/dist/css/layui.css')}">
+		<link rel="stylesheet" href="{:asset('static/common/css/ui.css')}" />
+		<link rel="stylesheet" href="{:asset('static/admin/css/other/login.css')}" />
+	</head>
+    <!-- 代 码 结 构 -->
+	<body>
+		<form class="layui-form" action="javascript:void(0);">
+			<div class="layui-form-item">
+				<img class="logo" src="{:asset('static/admin/images/logo.png')}" />
+				<div class="title">PHPCMS</div>
+				<div class="desc">
+                    企业级门户网站开发建设管理系统
+				</div>
+			</div>
+			<div class="layui-form-item">
+				<input placeholder="账 户 : admin " lay-verify="required" hover class="layui-input"  />
+			</div>
+			<div class="layui-form-item">
+				<input placeholder="密 码 : admin " lay-verify="required" hover class="layui-input"  />
+			</div>
+			<div class="layui-form-item">
+				<input placeholder="验证码 : "  hover  lay-verify="required" class="code layui-input layui-input-inline"  />
+				<img src="{:url('/captcha')}" class="codeImage" />
+			</div>
+			<div class="layui-form-item">
+				<input type="checkbox" name="" title="记住密码" lay-skin="primary" checked>
+			</div>
+			<div class="layui-form-item">
+				<button type="button" class="pear-btn pear-btn-success login" lay-submit lay-filter="login">
+					登 入
+				</button>
+			</div>
+		</form>
+		<!-- 资 源 引 入 -->
+        <script src="{:asset('assets/layui/dist/layui.js')}"></script>
+		<script src="{:asset('static/common/common.js')}"></script>
+		<script>
+			layui.use(['form', 'button', 'popup'], function() {
+				var form = layui.form;
+				var button = layui.button;
+				var popup = layui.popup;
+
+                // 登 录 提 交
+				form.on('submit(login)', function(data) {
+
+					/// 验证
+
+					/// 登录
+
+					/// 动画
+					button.load({
+						elem: '.login',
+						time: 1500,
+						done: function() {
+							popup.success("登录成功", function() {
+								location.href = "index.html"
+							});
+						}
+					})
+					return false;
+				});
+			})
+		</script>
+	</body>
 </html>
