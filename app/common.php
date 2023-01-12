2293f47ca310d76de5ed56debe43239628092088
wanganlin <inbox@wanganlin.com>
2023-01-12 14:12:13 +0800

    增加助手函数

--- app/common.php
+++ app/common.php
@@ -1,2 +1,24 @@
 <?php
 
+if (!function_exists('asset')) {
+    /**
+     * @param string $path
+     * @return string
+     */
+    function asset(string $path = ''): string
+    {
+        $root = request()->root(true);
+        return $root . '/' . ltrim($path, '/') . '?v=' . RELEASE;
+    }
+}
+
+if (!function_exists('theme')) {
+    /**
+     * @param string $path
+     * @return string
+     */
+    function theme(string $path = ''): string
+    {
+        return asset('themes/default/' . ltrim($path, '/'));
+    }
+}
