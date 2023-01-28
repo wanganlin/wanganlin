da66b5482e678b7c5a975ef9ebf805dd0327b2a4
wanganlin <inbox@wanganlin.com>
2023-01-28 16:22:02 +0800

    增加助手函数

--- app/common.php
+++ app/common.php
@@ -1,7 +1,37 @@
 <?php
 
+if (! function_exists('is_email')) {
+    /**
+     * 验证邮箱地址格式
+     *
+     * @param $email
+     * @return bool
+     */
+    function is_email($email): bool
+    {
+        return ! (filter_var($email, FILTER_VALIDATE_EMAIL) === false);
+    }
+}
+
+if (! function_exists('is_mobile')) {
+    /**
+     * 验证手机号码格式
+     *
+     * @param $mobile
+     * @return bool
+     */
+    function is_mobile($mobile): bool
+    {
+        $rule = '/^1[3-9]\d{9}$/';
+
+        return is_scalar($mobile) && 1 === preg_match($rule, (string) $mobile);
+    }
+}
+
 if (! function_exists('asset')) {
     /**
+     * 生成资源文件链接
+     *
      * @param  string  $path
      * @return string
      */
@@ -15,6 +45,8 @@ if (! function_exists('asset')) {
 
 if (! function_exists('theme')) {
     /**
+     * 主题文件链接
+     *
      * @param  string  $path
      * @return string
      */
