36cddb8aa0205a16e07a81c95bb04529784b0736
wanganlin <inbox@wanganlin.com>
2024-08-09 17:56:48 +0800

    wip

---
+++ java/project/ecboot/src/main/java/org/juling/ecboot/EcbootApplication.java
@@ -0,0 +1,13 @@
+package org.juling.ecboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcbootApplication.class, args);
+	}
+
+}
---
+++ java/project/ecboot/src/test/java/org/juling/ecboot/EcbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.juling.ecboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
